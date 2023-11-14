69c2c9797039f2fa72f70da0c3ce724c12a99726
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2023-11-15 01:55:19 +0700

    General Commit

--- src/components/Hero-section.jsx
+++ src/components/Hero-section.jsx
@@ -4,7 +4,8 @@ const HeroSection = () => {
 	return (
 		<div className="pt-44 px-sectionpxsm">
 			<p className="text-[45px] leading-none text-sand">
-				Unlock your startup's first move with our rapid design magic!
+				Accelerate your startup's first move with our rapid design
+				magic!
 			</p>
 			<p className="text-lg font-light mt-7 text-slate-300">
 				Discover a new way to kickstart your startup with our rapid

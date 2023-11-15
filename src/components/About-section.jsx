1193058eade87c34e37aa27836ecb50ca8a4d8f2
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2023-11-15 11:59:47 +0700

    About-section : updated

--- src/components/About-section.jsx
+++ src/components/About-section.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const AboutSection = () => {
-	return <div>AboutSection</div>;
+	return (
+		<div className="pt-40 px-sectionpxsm">
+			<p className="text-3xl leading-none text-sand">
+				When it comes to making your first move, we've got your back!
+			</p>
+			<p className="mt-10 text-xl font-extralight text-slate-300">
+				Creamypixel is a Design Company Agency helping startups to
+				unlock their moves. Say goodbye to the hassle of wasting your
+				time searching for freelancers and hello to lightning-fast
+				deliverables.
+			</p>
+			<p className="mt-5 text-xl font-extralight text-slate-300">
+				With us, it's all about prioritizing your business goals and
+				getting things done quickly.{" "}
+				<span className="italic">'pssst'</span> we always try to deliver
+				results that go beyond expectations!
+			</p>
+		</div>
+	);
 };
 
 export default AboutSection;

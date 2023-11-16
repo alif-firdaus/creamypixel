9e124f4d2536dfe6da2ef5ad91b80086fdf7853e
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2023-11-16 13:19:21 +0700

    GetstartedSection : updated

---
+++ src/components/GetstartedSection.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const GetstartedSection = () => {
+	return (
+		<div className="mt-40 px-sectionpxsm">
+			<p className="text-5xl leading-none text-sand">
+				See if cremypixel is{" "}
+				<span className="font-medium text-primary">legit</span> option
+				as it sounds
+			</p>
+			<p className="text-xl mt-9 font-extralight text-slate-300">
+				We're not waiting to be told, we perform and dive into the
+				matter to produce great outcomes.
+			</p>
+			{/* Main Button */}
+			<div className="flex items-center justify-center pt-5 pb-6 text-xl text-sand border-[1px] border-sand mt-14 rounded-xl">
+				Get started
+			</div>
+		</div>
+	);
+};
+
+export default GetstartedSection;
